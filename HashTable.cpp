cd0f1d561cde04dc834e9f27093f23eda0abddfd
microsandman <151220580+microsandman@users.noreply.github.com>
2025-10-25 21:24:03 -0400

    very start of .cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -4,3 +4,26 @@ october 25 2025
 HashTable
 Make a HashTable
 */
+
+#include "HashTable.h"
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+HashTable::HashTable(size_t initCapacity) {
+
+}
+
+bool HashTable::insert(const std::string$ key, const size_t &value) {
+
+}
+
+bool HashTable::remove(const std::string &key) {
+
+}
+
+bool HashTable::contains(const string &key) {
+
+}
+
