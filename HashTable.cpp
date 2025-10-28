49baaba708a605b0b9a6078fb040c05785ee885e
microsandman <151220580+microsandman@users.noreply.github.com>
2025-10-28 13:16:53 -0400

    i think this is it for the hash bucket

--- HashTable.cpp
+++ HashTable.cpp
@@ -10,14 +10,37 @@ Make a HashTable
 #include <exception>
 
 using namespace std;
+
+
+// Default constructor.
+HashTableBucket::HashTableBucket() : key(""), value(0), type(BucketType::ESS) {
+
+}
+
+//constructor.
+HashTableBucket::HashTableBucket(const std::string& key, const size_t& value) : key(key), value(value), type(BucketType::NORMAL) {
+
+}
+
+//Load for the bucket parameters
+void HashTableBucket::load(const std::string& key, const size_t& value) {
+    this->key = key;
+    this->value = value;
+    this->type = BucketType::NORMAL;
+}
+
+bool HashTableBucket::isEmpty() const {
+    return type == BucketType::ESS || type == BucketType::EAR;
+}
+
+
+
 /**
 * Only a single constructor that takes an initial capacity for the table is
 * necessary. If no capacity is given, it defaults to 8 initially
 */
 HashTable::HashTable(size_t initCapacity) {
-    for (size_t i = 0; i<initCapacity; i++) {
 
-    }
 }
 
 bool HashTable::insert(const std::string$ key, const size_t &value) {
